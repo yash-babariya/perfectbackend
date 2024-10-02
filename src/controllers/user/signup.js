a8a07ea11b62848c51bf016e8a2ab746a5a37a32
yash-babariya <yashbabariya549@gmail.com>
2024-10-02 10:53:08 +0530

    some changes...

--- src/controllers/user/signup.js
+++ src/controllers/user/signup.js
@@ -23,9 +23,9 @@ export default {
             const profilePicture = req.file?.filename;
             const hashedPassword = await bcrypt.hash(password, 10);
             const user = await User.create({ username, email, password: hashedPassword, profilePicture });
-            res.status(201).json({ message: "User created successfully", user });
+            return res.status(201).json({ message: "User created successfully", user });
         } catch (error) {
-            res.status(500).json({ message: error.message });
+            return res.status(500).json({ message: error.message });
         }
     }
 }
