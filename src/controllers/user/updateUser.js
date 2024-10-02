0485d9c0777c4128e05ae21217f6835ece69e56c
Yash Babariya <162944706+yash-babariya@users.noreply.github.com>
2024-10-02 15:16:46 +0530

    Update updateUser.js

--- src/controllers/user/updateUser.js
+++ src/controllers/user/updateUser.js
@@ -9,8 +9,15 @@ export default {
     },
     handler: async (req, res) => {
         try {
-            const user = await User.findByIdAndUpdate(req.user.id, req.body, { new: true });
-            res.status(200).json({ message: "User updated successfully", user });
+          const { username } = req.body;
+      const existingUser = await User.findOne({ username });
+      if (existingUser && existingUser._id.toString() !== req.user.id) {
+        return res.status(400).json({ message: "Username already exists" });
+      }
+      const user = await User.findByIdAndUpdate(req.user.id, req.body, {
+        new: true,
+      });
+      res.status(200).json({ message: "User updated successfully", user });
         } catch (error) {
             res.status(500).json({ message: "Internal server error" });
         }
