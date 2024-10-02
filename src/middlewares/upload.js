7bdcb1672ec2400db878495b34981e069520d331
yash-babariya <yashbabariya549@gmail.com>
2024-10-02 11:12:51 +0530

    some changes....

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, 'public');
     },
     filename: function (req, file, cb) {
         const username = req.body.username;
