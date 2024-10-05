44b00e86ed143fc94388daa2eb6e0773900d8f81
yash-babariya <yashbabariya549@gmail.com>
2024-10-05 11:36:06 +0530

    some changes....

--- src/controllers/user/updateUser.js
+++ src/controllers/user/updateUser.js
@@ -12,17 +12,20 @@ export default {
   handler: async (req, res) => {
     try {
       const { username } = req.body;
-      const existingUser = await User.findOne({ username });
-      if (existingUser && existingUser._id.toString() !== req.user.id) {
+      const existingUser = await User.findOne({ username, _id: { $ne: req.user.id } });
+      if (existingUser) {
         return responseHelper.badRequest(res, "Username already exists");
       }
-      const user = await User.findByIdAndUpdate(req.user.id, req.body, {
+      const user = await User.findByIdAndUpdate(req.user.id, { username }, {
         new: true,
       });
-      responseHelper.success(res, "User updated successfully", user);
+      responseHelper.success(res, "User updated successfully", user); o
     } catch (error) {
       responseHelper.internalServerError(res, error.message);
     }
   }
 }
+
+
+
 
